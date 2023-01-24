5fe723aa15954429e2855f85a87eba22c2e24045
Mauricio Londono <mauricio.londono@dhigroupinc.com>
2023-01-24 17:01:48 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HereoesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HereoesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
