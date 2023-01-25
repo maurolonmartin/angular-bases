0cb76e14735c3b5e5f7f3ca225069ba888dd8739
Mauricio Londono <mauricio.londono@dhigroupinc.com>
2023-01-24 20:15:05 -0500

    Servicio creado y app finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HereoesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ContadorModule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HereoesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
